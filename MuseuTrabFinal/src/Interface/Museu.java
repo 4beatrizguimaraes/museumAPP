6ca2ece8f1e13fd475c858924184e9daaffd3d83
Michelly Lima <43323869+MichellyLima@users.noreply.github.com>
2019-06-27 18:40:49 -0300

    Add files via upload

--- MuseuTrabFinal/src/Interface/Museu.java
+++ MuseuTrabFinal/src/Interface/Museu.java
@@ -7,4 +7,53 @@ public class Museu {
 	private Localizacao localizacao;
 	private Categoria categoria;
 	private ArrayList<ObradeArte> obras;
+	
+	
+	public Museu(String nome, Localizacao loc, Categoria categoria){
+		this.nome = nome;
+		this.localizacao = loc;
+		this.categoria = categoria;
+	}
+	
+	public void adicionaObra(ObradeArte obra){
+		obras.add(obra);
+		obra.setMuseu(this);
+	}
+	
+	public void adicionaObra(ArrayList<ObradeArte> obrasdeArte){
+		for(ObradeArte obra : obrasdeArte){
+			obras.add(obra);
+			obra.setMuseu(this);
+		}
+	}
+	
+	public void excluiObra(ObradeArte obra){
+		obras.remove(obra);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Localizacao getLocalizacao() {
+		return localizacao;
+	}
+
+	public void setLocalizacao(Localizacao localizacao) {
+		this.localizacao = localizacao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
 }
--- MuseuTrabFinal/src/Interface/ObradeArte.java
+++ MuseuTrabFinal/src/Interface/ObradeArte.java
@@ -3,5 +3,59 @@ package Interface;
 public class ObradeArte {
 	private String nome;
 	private String localizacao;
-	private static int codigo = 1;
+	private Categoria categoria;
+	private Autor autor;
+	private Museu museu;
+	private int codigo;
+	private int numcode = 1;
+	
+	public ObradeArte(String nome, String loc, Categoria categoria, Autor autor){
+		this.nome = nome;
+		this.localizacao = loc;
+		this.categoria = categoria;
+		this.codigo = numcode;
+		numcode++;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLocalizacao() {
+		return localizacao;
+	}
+
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+
+	public Museu getMuseu() {
+		return museu;
+	}
+
+	public void setMuseu(Museu museu) {
+		this.museu = museu;
+	}
+	
+	
 }
